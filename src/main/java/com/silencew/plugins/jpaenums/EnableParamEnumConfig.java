ac01a0f042331f36d72dc22c4d911780af5f05b7
wangshuiping <845146913@qq.com>
2021-01-19 11:39:13 +0800

    modify pom version and add enable annotations

---
+++ src/main/java/com/silencew/plugins/jpaenums/EnableParamEnumConfig.java
@@ -0,0 +1,18 @@
+package com.silencew.plugins.jpaenums;
+
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.*;
+
+/**
+ * 启用请求参数@PathVariable,@RequestParam等包含自定义枚举时转换配置
+ * Created by IntelliJ IDEA.
+ * author: wangshuiping
+ * date: 2021/1/19
+ */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Import(JsonEnumConfig.class)
+public @interface EnableParamEnumConfig {
+}
---
+++ src/main/java/com/silencew/plugins/jpaenums/JsonEnumDeserialze.java
@@ -0,0 +1,20 @@
+package com.silencew.plugins.jpaenums;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 请求参数@RequestBody等包含自定义枚举时转换注解
+ * Created by IntelliJ IDEA.
+ * author: wangshuiping
+ * date: 2021/1/19
+ */
+@Target({ElementType.ANNOTATION_TYPE, ElementType.METHOD, ElementType.FIELD, ElementType.TYPE, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+@JsonDeserialize(using = JsonEnumDeserializer.class)
+public @interface JsonEnumDeserialze {
+}
